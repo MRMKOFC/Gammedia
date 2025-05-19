100a7d6743055686f3d698daf15e75aa33142a55
MK <115228027+MRMKOFC@users.noreply.github.com>
2025-05-19 11:00:36 +0530

    Update gamerant_telegram_bot.py

--- gamerant_telegram_bot.py
+++ gamerant_telegram_bot.py
@@ -1,9 +1,4 @@
-import os 
-import requests 
-import time 
-from bs4 import BeautifulSoup 
-import logging 
-from datetime import datetime
+import os import requests import time from bs4 import BeautifulSoup import logging from datetime import datetime
 
 Setup logging
 
